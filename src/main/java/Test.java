77c1d6ec2761e90d4a0026476c6f95d8dcc5d4f0
ferraoiuxqrjj <157098630+ferraoiuxqrjj@users.noreply.github.com>
2024-02-21 15:43:46 +0800

    11

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName:Test
+ * @Auther: niuxiuming
+ * @Description:
+ * @Date: 2024/2/21 15:43
+ * @Version: v1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
